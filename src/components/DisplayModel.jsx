e03955fc7cdd0a032b9a78337c0ba776ea71a896
algomonk016 <stshandilya0820@gmail.com>
2022-11-19 08:59:04 +0530

    fixed rotation

--- src/components/DisplayModel.jsx
+++ src/components/DisplayModel.jsx
@@ -83,18 +83,18 @@ const DisplayModel = (props) => {
         </div>
       </DraggableBox>
 
-
       <Modal
         isOpen={isModalOpen}
         onRequestClose={closeModal}
         style={customStyles}
         contentLabel="Full Screen Modal"
+        shouldCloseOnEsc={false}
       >
 
         <div className='h-screen w-screen p-20 relative'>
           <Canvas camera={{ position: [30, 0, 30], fov: 3 }}>
-            <OrbitControls />
-            <mesh position={[0, -1, 0]}>
+            <OrbitControls minPolarAngle={Math.PI/2} maxPolarAngle={Math.PI/2} />
+            <mesh position={[0, -1, 0]} >
               <ambientLight intensity={1} />
               <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
               <Suspense fallback={null}>
